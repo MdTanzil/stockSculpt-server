453c2e72a4bcd7195823176297feced9d06c0072
MdTanzil <mdtanzil.py@gmail.com>
2025-02-08 21:18:27 +0600

    create asosiate route file

---
+++ model/Order.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop", required: true },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalAmount: { type: Number, required: true },
+  orderStatus: {
+    type: String,
+    enum: ["pending", "completed", "canceled"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-
 const userRouter = require("./userRoutes");
+const productRouter = require("./productRouter");
 
+// Add routes to the router
 router.use("/users", userRouter);
+router.use("/products", productRouter);
 
 module.exports = router;

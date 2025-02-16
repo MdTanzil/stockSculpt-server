4aa4a1b4d6816551c14c15cb415b9c4868aa7610
MdTanzil <mdtanzil.py@gmail.com>
2025-02-16 17:17:10 +0600

    add domain

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://localhost:5173", // Local frontend
-  "https://yourfrontenddomain.com", // Production frontend (Replace this)
+  "https://stocksculpt.web.app", // Production frontend (Replace this)
 ];
 
 const corsOptions = {
